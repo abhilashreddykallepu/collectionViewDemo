0d7ba8e027dba858351336ec478cdf32b98c1cf3
abhilashreddy <abhii.reddy6@gmail.com>
2014-03-10 11:47:13 +0530

    added project

---
+++ CollectionView/CollectionView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by Hadi Hatinoglu on 10/03/14.
+//  Copyright (c) 2014 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CollectionView/CollectionView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CollectionView
+//
+//  Created by Hadi Hatinoglu on 10/03/14.
+//  Copyright (c) 2014 Baraansoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
